7c2cf11c9ec9061bd0a54f07e5188884670932d7
Jiajin Yang <jiajinyang@JiajindeMacBook-Pro.local>
2019-05-14 14:55:32 +0800

    heh

--- src/views/attendance/attendance.vue
+++ src/views/attendance/attendance.vue
@@ -35,7 +35,7 @@ export default {
       value: '',
       map : {},
       center: {
-        lng: '116.655227',
+        lng: '116.655228',
         lat: '39.956155'
       },
       nowPosition: {
